3dec0594636d174c7fd3ba3e78cf182d33673da2
k-kukes <158488017+k-kukes@users.noreply.github.com>
2025-04-16 21:25:57 -0400

    Created MemberManager Skeleton

---
+++ GymMembershipManagementSystem/src/main/java/org/example/MemberManager.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class MemberManager {
+    public MemberManager(){
+
+    }
+
+    public void processPayment(){
+        // Process their monthly payment
+    }
+
+    public void switchMembership(){
+        // Switch gym membership type from premium to regular or regular to premium
+    }
+
+    public void cancelMembership(){
+        // Process cancelling membership
+    }
+}
