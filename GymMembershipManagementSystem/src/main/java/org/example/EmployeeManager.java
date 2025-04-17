7a1a47e76fb9c48bf434bd21d6681b1937d4cf32
k-kukes <158488017+k-kukes@users.noreply.github.com>
2025-04-16 21:29:43 -0400

    Create EmployeeManager Skeleton

---
+++ GymMembershipManagementSystem/src/main/java/org/example/EmployeeManager.java
@@ -0,0 +1,27 @@
+package org.example;
+
+public class EmployeeManager {
+    public EmployeeManager(){
+
+    }
+
+    public void addNewMember(){
+        // to add members
+    }
+
+    public void removeMember(){
+        //to remove a member
+    }
+
+    public void manageMembersInfo(){
+        // to manage information about a member
+    }
+
+    public void addEmployee(){
+        // to add employee
+    }
+
+    public void removeEmployee(){
+        //to remove employee
+    }
+}
